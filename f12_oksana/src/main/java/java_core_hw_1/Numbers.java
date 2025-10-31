44d4baa7e94d7025fabeed2a9f5b1cc85109060b
ksenaf <fobs2009@gmail.com>
2025-10-31 18:31:08 +0200

    numbers edits

--- f12_oksana/src/main/java/java_core_hw_1/Numbers.java
+++ f12_oksana/src/main/java/java_core_hw_1/Numbers.java
@@ -8,27 +8,23 @@ public class Numbers {
         Scanner scanner = new Scanner(System.in);
 
         Random random = new Random();
-        int randomNumber = random.nextInt(101); //Random number from 0 to 100
-
-        //System.out.println(RandomNumber);
+        int randomNumber = random.nextInt(101);
 
         System.out.println("Please enter your name: ");
         String playerName = scanner.nextLine(); //The player enters a name
 
         System.out.println("Let the game begin!");
 
-        // Infinite loop
         while (true) {
             System.out.print("Enter a number from 0 and 100: ");
 
-            // Checking if an integer is entered
             if (!scanner.hasNextInt()) {
                 System.out.println("This is not a whole number! Try again.");
                 scanner.next();
                 continue;
             }
 
-            int playerNumber = scanner.nextInt(); //The player enters a number
+            int playerNumber = scanner.nextInt();
 
             if (playerNumber < randomNumber) {
                 System.out.println("Your number is too small. Please, try again.");
@@ -36,7 +32,7 @@ public class Numbers {
                 System.out.println("Your number is too big. Please, try again.");
             } else {
                 System.out.println("Congratulations, " + playerName + "!");
-                break; // Number guessed - exit the loop
+                break;
             }
         }
         scanner.close();
