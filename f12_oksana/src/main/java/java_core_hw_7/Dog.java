a9363b3b7684fbbec0652c2fff53ebfe9bf9aead
ksenaf <fobs2009@gmail.com>
2025-11-16 00:52:48 +0200

    happy family 4

---
+++ f12_oksana/src/main/java/java_core_hw_7/Dog.java
@@ -0,0 +1,24 @@
+package java_core_hw_7;
+
+import java.util.Set;
+
+public class Dog extends Pet implements Foul {
+    public Dog(String nickname, int age, int trickLevel, Set<String> habits) {
+        super(nickname, age, trickLevel, habits);
+    }
+
+    @Override
+    protected Species defineSpecies() {
+        return Species.DOG;
+    }
+
+    @Override
+    public void respond() {
+        System.out.println("I am a dog " + getNickname());
+    }
+
+    @Override
+    public void foul() {
+        System.out.println(getNickname() + " did abomination!");
+    }
+}
